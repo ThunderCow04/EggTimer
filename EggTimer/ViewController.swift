ece8105ca04bdc6643c4d9a78c98df477cc425d9
Kirill Gerasin <Kirill Gerasin>
2020-06-24 16:10:20 +0300

    Project was added

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
+    
+    @IBAction func HardnessSelected(_ sender: UIButton) {
+        
+        progressBar.progress = 0.5
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        totalTime  = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+
+            let percentageProgess = Float(secondsPassed) / Float(totalTime)
+            
+            progressBar.progress = percentageProgess
+        }
+        else{
+            timer.invalidate()
+            titleLabel.text = "Done"
+            playSound()
+                
+            }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+}
